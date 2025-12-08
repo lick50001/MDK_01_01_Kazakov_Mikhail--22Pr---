a48a73fe08efa186bca8a89a6db9c6da090b6b1c
qwelord <secondofall22@rambler.ru>
2025-12-08 14:37:52 +0500

    Разработанный словарь.Казаков

--- PhoneBook_Kazakov/Pages/Main.xaml.cs
+++ PhoneBook_Kazakov/Pages/Main.xaml.cs
@@ -24,5 +24,15 @@ namespace PhoneBook_Kazakov.Pages
         {
             InitializeComponent();
         }
+
+        private void Click_Phone(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Click_History(object sender, MouseButtonEventArgs e)
+        {
+
+        }
     }
 }
