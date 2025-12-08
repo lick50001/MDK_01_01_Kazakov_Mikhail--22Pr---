b02c5ac85e83f8a4798ddf7cf8e754d352099ecf
qwelord <secondofall22@rambler.ru>
2025-12-08 15:19:49 +0500

    Разработанные классы. Казаков

---
+++ ClassModule/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassModule
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string phone_num { get; set; }
+        public string fio_user { get; set; }
+        public string passport_data { get; set; }
+    }
+}
