d11580a2c9dabdf6da1fd01dfd36f3fdd8b02fcb
qwelord <secondofall22@rambler.ru>
2025-12-08 14:46:09 +0500

    Разработанное окно MainWindow. Казаков

--- PhoneBook_Kazakov/MainWindow.xaml.cs
+++ PhoneBook_Kazakov/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +21,40 @@ namespace PhoneBook_Kazakov
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static Connection connect;
+        public static Pages.Main main;
         public MainWindow()
         {
             InitializeComponent();
+            connect = new Connection();
+
+            connect.LoadData(Connection.tabels.users);
+            connect.LoadData(Connection.tabels.calls);
+
+            main = new Pages.Main();
+
+            OpenPageMain();
+        }
+
+        public void OpenPageMain()
+        {
+            DoubleAnimation opgriAnimation = new DoubleAnimation();
+            opgriAnimation.From = 1;
+            opgriAnimation.To = 0;
+            opgriAnimation.Duration = TimeSpan.FromSeconds(0.6);
+            opgriAnimation.Completed += delegate
+            {
+                frame.Navigate(main);
+
+                DoubleAnimation opgrisAnimation = new DoubleAnimation();
+                opgrisAnimation.From = 0;
+                opgrisAnimation.To = 1;
+                opgrisAnimation.Duration = TimeSpan.FromSeconds(1.2);
+
+                frame.BeginAnimation(Frame.OpacityProperty, opgriAnimation);
+            };
+
+            frame.BeginAnimation(Frame.OpacityProperty, opgriAnimation);
         }
     }
 }
